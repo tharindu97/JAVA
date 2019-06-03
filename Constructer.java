0f98a8288abb5d8d9ed7d5c76cef12b873aec34c
tharindu97 <39029828+tharindu97@users.noreply.github.com>
2019-06-04 00:23:51 +0530

    JAVA
    java

---
+++ Constructer.java
@@ -0,0 +1,16 @@
+class Constructer{
+    public static void main(String []ary){
+        Car ob = new Car();
+
+    }
+}
+
+class Car{
+   Car(){
+       System.out.println("my name is tharindu");
+   }
+
+   void Main(){
+       System.out.println(" 2 ");
+   }
+}
